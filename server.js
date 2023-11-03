36edf8645c2626ffd4b7311f4ea0c2097e84183e
Andrew Samaha <andrew.samaha@gmail.com>
2023-11-03 16:10:38 -0400

    feat: adds express

---
+++ server.js
@@ -0,0 +1,10 @@
+import 'dotenv/config';
+import express from "express";
+import ViteExpress from "vite-express";
+
+const app = express();
+ViteExpress.config({ mode: process.env.NODE_ENV })
+
+app.get("/message", (_, res) => res.send("Hello from express!"));
+
+ViteExpress.listen(app, 3000, () => console.log("Server is listening..."));
